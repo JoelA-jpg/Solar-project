4ce8b8a96bd39f90c652c233524676cd59195e24
JoelA-jpg <hotmail.ja@gmail.com>
2025-10-03 09:50:53 +0200

    .h version

--- Control test code/p_func.h
+++ Control test code/p_func.h
@@ -1,1 +1,15 @@
-double p_func(double V_diff, double K);
+#ifndef P_FUNC_H
+#define P_FUNC_H
+
+static inline double p_func(double V_diff, double K){
+    double output = K * V_diff*0.4*500;//normalisation for 2.5V Ain, 500 us Aut
+    //prevent out of bounds
+    if(output>500){
+        output = 500;
+    }
+    if(output<-500){
+        output = -500;
+    } 
+    return 1.5 + 0.001*output;      
+}
+#endif
---
+++ Control test code/p_test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "p_func.h"
+
+int main(){
+    const int N=20;
+    double K = 1;
+    double input[N];
+    double output[N];
+
+    for(int i=0;i<N;i++){
+        input[i] = 2.5 + 1.5*sin(i);
+        output[i] = p_func(input[i]-2.5, K);
+    }
+
+    for (int i = 0; i < N; i++) {
+        printf("%10.5f  %10.5f\n", input[i], output[i]);
+    }
+
+    return 0;
+}
