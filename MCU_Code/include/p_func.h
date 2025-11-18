4ef62d26b6d38591e6946fb6465c3161cd8edf57
Krukan-Ki <adrian.garde@telia.com>
2025-11-18 15:49:33 +0100

    Update p_func.h
    Changed 20 to 40.

--- MCU_Code/include/p_func.h
+++ MCU_Code/include/p_func.h
@@ -23,7 +23,7 @@ static inline uint16_t p_funcy(uint16_t pos, uint16_t V_meas, int16_t K){
     int16_t diff = (int16_t)(V_meas - 512);
     
     int32_t num   = (int32_t)2 * (int32_t)K * (int32_t)diff;
-    int32_t delta = (num + (num >= 0 ? 2 : -2)) / 20;
+    int32_t delta = (num + (num >= 0 ? 2 : -2)) / 40;
     int32_t output = pos + delta;
     //prevent out of bounds
     if(output>2500){
