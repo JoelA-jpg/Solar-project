bb7088a7d883450b0a3e2faf3dae7927ae6f221e
JoelA-jpg <hotmail.ja@gmail.com>
2026-01-08 21:44:45 +0100

    sista koden

--- MCU_Code/include/p_func.h
+++ MCU_Code/include/p_func.h
@@ -62,18 +62,18 @@ static inline uint16_t MPPT_pulse(float P_old, float P_new, float I_old,
     if(d_V > dVeps || d_V < -dVeps){ //(d_V) (d_V > dVeps || d_V < -dVeps)
         //int16_t ineq = d_P/d_V;
         if((d_P > 0 && d_V > 0) || (d_P < 0 && d_V < 0)){ // (d_P > 2*d_V) alt if ((d_P > 0 && d_V > 0) || (d_P < 0 && d_V < 0))
-            MPPT_pw = MPPT_pw + step;
+            MPPT_pw = MPPT_pw - step;
         }
         else if ((d_P > 0 && d_V < 0) || (d_P < 0 && d_V > 0)){ // (d_P < -2*d_V) alt if ((d_P > 0 && d_V < 0) || (d_P < 0 && d_V > 0))
-            MPPT_pw = MPPT_pw - step;
+            MPPT_pw = MPPT_pw + step;
         }
     }
     else{
         if (d_I > dIeps){
-            MPPT_pw = MPPT_pw + step;
+            MPPT_pw = MPPT_pw - step;
         }
         else if (d_I < -dIeps){
-            MPPT_pw = MPPT_pw - step;
+            MPPT_pw = MPPT_pw + step;
         }
     }
 
